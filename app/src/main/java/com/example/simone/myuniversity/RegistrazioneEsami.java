49bb512df7bf2c0e7d48ee4a8ccb62b34e6f2ade
Simone <simone.dema@hotmail.it>
2016-02-05 18:21:47 +0100

    Implementare Activity RegistrazioneEsami

--- app/src/main/java/com/example/simone/myuniversity/RegistrazioneEsami.java
+++ app/src/main/java/com/example/simone/myuniversity/RegistrazioneEsami.java
@@ -2,12 +2,17 @@ package com.example.simone.myuniversity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class RegistrazioneEsami extends AppCompatActivity {
+
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrazione_esami);
+
+
     }
 }
