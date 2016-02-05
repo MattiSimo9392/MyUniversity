29a041d5ff28a14150436d47e22953efecbc71f5
Simone <simone.dema@hotmail.it>
2016-02-05 16:47:00 +0100

    Creata activity RegistrazioneEsami

--- app/src/main/java/com/example/simone/myuniversity/Menu.java
+++ app/src/main/java/com/example/simone/myuniversity/Menu.java
@@ -64,6 +64,10 @@ public class Menu extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), GestionePrenotazioneEsami.class));
     }
 
+    public void onClick_regExam(View view){
+        startActivity(new Intent(getApplicationContext(), RegistrazioneEsami.class));
+    }
+
     public void onClick_CancelCareer(View view){
         // Riportiamo la preference al valore iniziale in modo che compaia il bottone
         /*
---
+++ app/src/main/java/com/example/simone/myuniversity/RegistrazioneEsami.java
@@ -0,0 +1,13 @@
+package com.example.simone.myuniversity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrazioneEsami extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrazione_esami);
+    }
+}
